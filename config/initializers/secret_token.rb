e28df1f19a6f5d5609187b315bab87eae6211930
Nathanael Miksis <nathanael.miksis@gmail.com>
2013-03-19 14:00:33 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WholeEquine::Application.config.secret_token = 'b379da93b21b3e8cc82afb3af0210dd435e9fdd907bf0b08b0191eef262766e6001aa7222be1e86938fb5c1b72d977fc3c84259415700054326f3bfc72958927'
